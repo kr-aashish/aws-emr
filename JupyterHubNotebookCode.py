f7e84d0ef636ad60519dadd8e3b89fb19b65aeac
Aashish Kumar <jmphedaashish@gmail.com>
2024-09-28 08:08:06 +0530

    Add S3 bucket name to CLI scripts

--- JupyterHubNotebookCode.py
+++ JupyterHubNotebookCode.py
@@ -5,8 +5,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
 # Step 2
-input_path = "s3://<YourS3BucketName>/input/tripdata.csv"
-output_path = "s3://<YourS3BucketName>/output/"
+input_path = "s3://cgs-emr-jobs/input/tripdata.csv"
+output_path = "s3://cgs-emr-jobs/output/"
 
 # Step 3
 nyTaxi = spark.read.option("inferSchema", "true").option("header", "true").csv(input_path)
